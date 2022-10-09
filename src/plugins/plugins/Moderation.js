200522aea4e891bcaa2354a4879e78afde25ddab
Antonio Vallez <arvallez113@students.ecsu.edu>
2022-10-09 14:29:43 -0400

    Pushing

--- src/plugins/plugins/Moderation.js
+++ src/plugins/plugins/Moderation.js
@@ -8,7 +8,9 @@ export default class Moderation extends Plugin {
         this.events = {
             'mute_player': this.mutePlayer,
             'kick_player': this.kickPlayer,
-            'ban_player': this.banPlayer
+            'ban_player': this.banPlayer,
+            'stealth_mode': this.stealthMode,
+            'warn_player': this.warnPlayer
         }
     }
 
@@ -24,8 +26,37 @@ export default class Moderation extends Plugin {
         let recipient = this.usersById[args.id]
 
         if (recipient && recipient.data.rank < user.data.rank) {
+			recipient.send('close_with_error', {error: 'You have been kicked by a moderator. Please make sure to follow the CPF rules.'})
             recipient.close()
+            this.discord.kickLogs(user.data.username, recipient.data.username)
         }
+    }
+
+    warnPlayer(args, user) {
+        if (!user.isModerator) {
+            return
+        }
+
+        let recipient = this.usersById[args.id]
+
+        if (recipient && recipient.data.rank < user.data.rank) {
+            recipient.send('error', {
+                error: `You have been warned by a moderator!\nPlease follow the rules or you may be banned!`
+            })()
+        }
+    }
+
+    stealthMode(args, user) {
+        if (!user.isModerator) {
+            return
+        }
+
+        this.db.users.update({ stealthMode: (args.stealthMode) ? 0 : 1 }, { where: { id: user.data.id } })
+
+        let string = args.stealthMode ? 'disabled' : 'enabled'
+        user.send('error', {
+            error: `Stealth mode ${string}\nPlease relogin.`
+        })()
     }
 
     async banPlayer(args, user) {
@@ -40,6 +71,7 @@ export default class Moderation extends Plugin {
             await this.applyBan(user, args.id)
 
             if (recipient) {
+				recipient.send('close_with_error', {error: 'You have been banned. Please make sure to follow the CPF rules.'})
                 recipient.close()
             }
         }
@@ -55,6 +87,10 @@ export default class Moderation extends Plugin {
         }
 
         this.db.bans.create({ userId: id, expires: expires, moderatorId: moderator.data.id, message: message })
+
+        let userName = (await this.db.getUserById(id)).username
+
+        this.discord.banLogs(moderator.data.username, userName, expires)
     }
 
     async getRecipientRank(recipient, id) {
---
+++ src/plugins/plugins/Panel.js
@@ -0,0 +1,258 @@
+import Plugin from '../Plugin'
+
+
+export default class Panel extends Plugin {
+
+    constructor(users, rooms) {
+        super(users, rooms)
+        this.events = {
+            'get_unverified_users': this.getUnverifedUsers,
+            'verify_user': this.verifyUser,
+            'reject_user': this.rejectUser,
+            'get_user_info': this.getUserInfo,
+            'edit_player': this.editPlayer,
+            'add_user_coins': this.addUserCoins,
+            'add_user_items': this.addUserItems,
+            'ban_user': this.banUser,
+            'change_user_name': this.changeUsername
+        }
+    }
+
+    async getUnverifedUsers(args, user) {
+
+        if (user.data.rank < 4) {
+            user.send('error', {
+                error: 'You do not have permission to perform this action.'
+            })
+			return
+        }
+
+        let users = await this.db.getUnverifedUsers()
+
+        if (users) {
+            user.send('get_unverified_users', {
+                users: users
+            })
+        }
+    }
+
+    async verifyUser(args, user) {
+
+        if (user.data.rank < 4) {
+            user.send('error', {
+                error: 'You do not have permission to perform this action.'
+            })
+			return
+        }
+
+        await this.db.users.update({
+            username_approved: 1,
+            username_rejected: 0
+        }, {
+            where: {
+                id: args.id
+            }
+        })
+
+        this.getUnverifedUsers(args, user)
+    }
+
+    async rejectUser(args, user) {
+
+        if (user.data.rank < 4) {
+            user.send('error', {
+                error: 'You do not have permission to perform this action.'
+            })
+			return
+        }
+
+        await this.db.users.update({
+            username_rejected: 1,
+            username_approved: 0
+        }, {
+            where: {
+                id: args.id
+            }
+        })
+
+        this.getUnverifedUsers(args, user)
+    }
+
+    async getUserInfo(args, user) {
+
+        if (user.data.rank < 4) {
+            user.send('error', {
+                error: 'You do not have permission to perform this action.'
+            })
+			return
+        }
+
+        let users = await this.db.searchForUsers(args.username)
+
+        if (users) {
+            user.send('get_unverified_users', {
+                users: users
+            })
+        }
+
+    }
+
+    async editPlayer(args, user) {
+
+        if (user.data.rank < 4) {
+            user.send('error', {
+                error: 'You do not have permission to perform this action.'
+            })
+			return
+        }
+
+        let request = await this.db.getUserById(args.id)
+
+        request.ip = null
+        request.loginKey = null
+        request.password = null
+
+        let bans = await this.db.getBanCount(args.id)
+        let activeban =  await this.db.getActiveBan(args.id)
+
+        if (request) {
+            user.send('edit_player', {
+                user: request,
+                bancount: bans,
+                activeban: activeban
+            })
+        }
+    }
+
+    async addUserCoins(args, user) {
+
+        if (user.data.rank < 4) {
+            user.send('error', {
+                error: 'You do not have permission to perform this action.'
+            })
+			return
+		}
+
+        let userName = (await this.db.getUserById(args.id)).username
+
+        user.send('error', {
+            error: 'Coins added successfully.'
+        })
+
+        let recipient = this.usersById[args.id]
+
+
+        if (recipient) {
+            await recipient.updateCoins(parseInt(args.coins))
+            recipient.send('end_ruffle_mingame', { coins: recipient.data.coins, game: 'Gift from a moderator!', coinsEarned: args.coins, stamps: recipient.stamps.list })
+        }
+        else {
+            await this.db.addCoins(args.id, args.coins)
+        }
+
+        this.discord.addCoinLogs(user.data.username, userName, args.coins)
+    }
+
+    async addUserItems(args, user) {
+
+        if (user.data.rank < 4) {
+            user.send('error', {
+                error: 'You do not have permission to perform this action.'
+            })
+			return
+        }
+
+        let userName = (await this.db.getUserById(args.id)).username
+
+        this.discord.addItemLogs(user.data.username, userName, args.itemName)
+
+        let recipient = this.usersById[args.id]
+
+        args.item = parseInt(args.item)
+
+        if (recipient) {
+            let item = this.crumbs.items[args.item]
+            recipient.inventory.add(args.item)
+            recipient.send('add_item', { item: args.item, name: args.itemName, slot: this.crumbs.items.slots[item.type - 1], coins: recipient.data.coins })
+            user.send('error', {
+                error: 'Item added successfully.'
+            })
+        }
+        else {
+            let item = this.db.addItem(args.id, args.item)
+
+            if (item) {
+                user.send('error', {
+                    error: 'Item added successfully.'
+                })
+            }
+        }
+    }
+
+    async banUser(args, user) {
+        if (user.data.rank < 4) {
+            user.send('error', {
+                error: 'You do not have permission to perform this action.'
+            })
+			return
+        }
+        let recipient = this.usersById[args.id]
+        let recipientRank = await this.getRecipientRank(recipient, args.id)
+
+        if (recipientRank < user.data.rank) {
+            let date = new Date()
+            let expiry = date.getTime() + args.banDuration
+            await this.db.ban(args.id, expiry, user.data.id)
+
+            let userName = (await this.db.getUserById(args.id)).username
+            let expiryDate = new Date(expiry)
+			
+			if (recipient) {
+				recipient.send('close_with_error', {error: `You have been banned until ${expiryDate.toUTCString()}. Please make sure to follow the CPF rules.`})
+                recipient.close()
+            }
+
+            user.send('error', {
+                error: `Player banned until ${expiryDate.toUTCString()}`
+            })
+
+            this.discord.banLogs(user.data.username, userName, args.durationText, expiryDate.toUTCString())
+        }
+
+        else {
+            user.send('error', {
+                error: 'This user has the same permission level as you.'
+            })
+        }     
+    }
+
+    async changeUsername(args, user) {
+
+        if (user.data.rank < 4) {
+            user.send('error', {
+                error: 'You do not have permission to perform this action.'
+            })
+			return
+        }
+
+        let userName = (await this.db.getUserById(args.id)).username
+
+        let complete = await this.db.changeUsername(args.id, args.newUsername)
+
+        if (complete) {
+            user.send('error', {
+                error: 'Username changed successfully.'
+            })
+
+            this.discord.changeUsernameLogs(user.data.username, userName, args.newUsername)
+        }
+
+    }
+
+    async getRecipientRank(recipient, id) {
+        return (recipient)
+            ? recipient.data.rank
+            : (await this.db.getUserById(id)).rank
+    }
+
+}
